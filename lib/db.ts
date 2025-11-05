7f297da0a178451ead5ceba6b6a426b448ac6196
Robin5605 <robinjefferson123@gmail.com>
2025-11-04 22:12:21 -0600

    Add method to fetch all menu items

--- lib/db.ts
+++ lib/db.ts
@@ -4,3 +4,8 @@ import { MenuItem } from './models';
 const client = new Client();
 await client.connect();
 
+export async function fetch_all_menu_items(): Promise<MenuItem[]> {
+    const result = await client.query<MenuItem>("SELECT * FROM menu");
+
+    return result.rows;
+}

1c7ce70e672da16a00db3d71358ce66e4da4442c
PaninGitHub <diniduk@outlook.com>
2025-11-05 13:14:01 -0600

    feat: Initalized ItemCard  component

---
+++ app/ItemCard.tsx
@@ -0,0 +1,15 @@
+import React, { memo } from 'react';
+
+interface ItemCardProps {
+  itemName: string;
+}
+
+const ItemCard: React.FC<ItemCardProps> = memo(({ itemName }) => {
+  return (
+    <div className="h-[100px] w-[100px] bg-gray-200 flex justify-center items-center shadow-lg p-2 text-center">
+      <p className="whitespace-normal wrap-break-words">{itemName}</p>
+    </div>
+  );
+});
+
+export default ItemCard;

5de0bd22b4a0b70650ffa4a158438576d6eb7af7
ZanirP <46696445+ZanirP@users.noreply.github.com>
2025-11-05 09:38:06 -0600

    docs: Added documentation for fetch_all_menu_items

--- lib/db.ts
+++ lib/db.ts
@@ -4,8 +4,14 @@ import { MenuItem } from './models';
 const client = new Client();
 await client.connect();
 
+
+/**
+ * Fetches all menu items from the database.
+ * @return A promise that resolves to an array of MenuItem objects.
+ */
 export async function fetch_all_menu_items(): Promise<MenuItem[]> {
     const result = await client.query<MenuItem>("SELECT * FROM menu");
 
     return result.rows;
 }
+

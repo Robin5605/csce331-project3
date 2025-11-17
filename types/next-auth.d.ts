81b42954bbdcc133f337a293e7e8c7cc2367abe5
ZanirP <46696445+ZanirP@users.noreply.github.com>
2025-11-17 15:35:01 -0600

    feat: sessions should be implemented during login, roles should be assigned

---
+++ types/next-auth.d.ts
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth";
+
+export type Role = "manager" | "cashier" | "customer";
+
+declare module "next-auth" {
+  interface User {
+    id: string;
+    role: Role;
+  }
+
+  interface Session {
+    user: {
+      id: string;
+      role: Role;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    role: Role;
+  }
+}

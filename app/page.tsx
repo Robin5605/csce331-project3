581b18f0ebe293631105cb43b961ca6ddd0b0cca
Karam Masad <karammasad@tamu.edu>
2025-12-03 21:29:38 -0600

    feat: added translation for home page.

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import { useRouter } from "next/navigation";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
 
+import GoogleTranslate from "@/components/GoogleTranslate";
+
 export default function Home() {
     const router = useRouter();
 
@@ -21,6 +23,11 @@ export default function Home() {
                     />
                 </div>
             </nav>
+
+            <div className="flex justify-end m-4">
+                <GoogleTranslate />
+            </div>
+           
 
             {/* PAGE CONTENT */}
             <main className="mx-auto flex w-full max-w-6xl flex-col gap-10 px-4 py-10 md:flex-row md:items-center md:justify-between">
